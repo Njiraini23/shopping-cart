ac9e5fefbeb83de954b6bd4e32216086847c21a8
Njiraini23 <njiraini88@gmail.com>
2024-11-01 13:29:25 +0300

    Adding the cart loading functionality

--- shopping/src/context/index.jsx
+++ shopping/src/context/index.jsx
@@ -6,12 +6,16 @@
 //consume the context using useContext
 
 import { createContext, useEffect, useState } from "react";
+import {useNavigate} from 'react-router-dom';
 
 export const ShoppingCartContext = createContext(null);
 
 function ShoppingCartProvider({children}){
     const [loading, setLoading] = useState(true);
     const [listOfProducts, setListOfProducts] = useState([]);
+    const [productDetails, setProductDetails] = useState(null);
+    const [cartItems, setCartItems] = useState([]); 
+    const navigate = useNavigate() 
     
     async function fetchListOfProducts(){
         const apiResponse = await fetch('https://dummyjson.com/products');
@@ -25,13 +29,46 @@ function ShoppingCartProvider({children}){
         }
     }
 
+    function handleAddToCart(getProductDetails){
+        console.log(getProductDetails);
+
+        let cpyExistingCartItems = [...cartItems];
+        const findIndexOfCurrentItem = cpyExistingCartItems.findIndex(cartItem=>cartItem.id === getProductDetails.id);
+
+        console.log(findIndexOfCurrentItem);
+
+        if(findIndexOfCurrentItem === -1){
+            cpyExistingCartItems.push({
+                ...getProductDetails,
+                quantity : 1,
+                totalPrice: getProductDetails?.price
+            })
+        } else {
+
+        }
+        console.log(cpyExistingCartItems, "cpyExistingCartItems");
+        setCartItems(cpyExistingCartItems);
+        localStorage.setItem('cartItems', JSON.stringify(cpyExistingCartItems));
+        navigate('/cart'); 
+        
+    }
+
    useEffect(()=>{
     fetchListOfProducts()
    }, []);
    
-   console.log(listOfProducts);
+   console.log(cartItems);
 
-    return <ShoppingCartContext.Provider value={{ listOfProducts, loading }}>
+    return <ShoppingCartContext.Provider
+     value={{ 
+        listOfProducts, 
+        loading, 
+        setLoading, 
+        productDetails, 
+        setProductDetails,
+        handleAddToCart,
+        cartItems, 
+        }}>
         {children}
         </ShoppingCartContext.Provider>
 }
